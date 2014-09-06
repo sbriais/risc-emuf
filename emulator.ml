a02400ebbf78332cff88ecefa6e6d9db8327aa19
Sebastien Briais <sbriais@free.fr>
2014-09-06 13:10:02 +0200

    *** empty log message ***
    git-svn-id: svn://obiwan/Work/trunk@1386 43105fab-dcc8-4d00-bdf7-3c9d691a82a3

--- emulator.ml
+++ emulator.ml
@@ -1,5 +1,7 @@
 open Bigarray
 
+exception BusError of int
+
 class emulator = 
   let create_memory mem_size = 
     let mem = Array1.create int32 c_layout mem_size in
@@ -17,6 +19,31 @@ object(self)
     code#iter 
       (fun i instr ->
 	 memory.{i} <- Codec.code_instruction instr)
+  method readWord adr =
+    if adr land 0x3 <> 0 then raise (BusError(adr))
+    else
+      try
+	memory.{adr lsr 2}
+      with Invalid_argument(_) -> raise (BusError(adr))
+  method readByte adr =
+    let b = self#readWord ((adr lsr 2) lsl 2) in
+    let num = 3 - (adr land 0x3) in
+      Int32.logand (Int32.shift_right_logical b (8*num)) (Int32.of_int 0xff)
+  method writeWord adr w =
+    if adr land 0x3 <> 0 then raise (BusError(adr))
+    else
+      try
+	memory.{adr lsr 2} <- w
+      with Invalid_argument(_) -> raise (BusError(adr))
+  method writeByte adr b =
+    let w = self#readWord ((adr lsr 2) lsl 2) in
+    let num = 3 - (adr land 0x3) in
+    let mask = Int32.lognot (Int32.shift_left (Int32.of_int 0xff) (8*num)) in
+    let w = Int32.logand w mask in
+    let w = Int32.logor w (Int32.shift_left (Int32.logand b (Int32.of_int 0xff)) (8*num)) in
+      self#writeWord ((adr lsr 2) lsl 2) w
+  method private fetch =
+    Codec.decode_instruction (self#readWord pc)
   initializer
     self#init
 end
--- main.ml
+++ main.ml
@@ -4,4 +4,4 @@ let _ =
   let scanner = new Scanner.scanner (Scanner.charReader_of_in_channel stdin) in
   let code = Analyzer.analyze_program scanner in
   let emulator = new Emulator.emulator code (!mem_size) in
-    code#dump
+    ()
